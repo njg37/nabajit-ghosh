41a23206508959244902fd820f6f322d2ceac185
NjG19o2 <njg19o2@gmail.com>
2025-02-11 20:41:06 +0530

    scroll option

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,55 +1,91 @@
 import React from "react";
 import "./Header.css";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-scroll"; // Import react-scroll
 
 const Header = () => {
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
   return (
     <header className="header">
       <nav className="navbar">
-        <NavLink
-          to="/nabajit-ghosh/"
+        <Link
+          to="home"
           className="nav-item"
           title="Home"
-          onClick={scrollToTop}
+          smooth={true}
+          duration={500}
         >
           <i className="fas fa-house-user"></i>
           <span className="nav-tooltip">Home</span>
-        </NavLink>
-        <NavLink to="/about" className="nav-item" title="About">
+        </Link>
+        <Link
+          to="about"
+          className="nav-item"
+          title="About"
+          smooth={true}
+          duration={500}
+        >
           <i className="fas fa-address-card"></i>
           <span className="nav-tooltip">About</span>
-        </NavLink>
-        <NavLink to="/experience" className="nav-item" title="Experience">
+        </Link>
+        <Link
+          to="experience"
+          className="nav-item"
+          title="Experience"
+          smooth={true}
+          duration={500}
+        >
           <i className="fas fa-briefcase"></i>
           <span className="nav-tooltip">Experience</span>
-        </NavLink>
-        <NavLink to="/education" className="nav-item" title="Education">
+        </Link>
+        <Link
+          to="education"
+          className="nav-item"
+          title="Education"
+          smooth={true}
+          duration={500}
+        >
           <i className="fas fa-graduation-cap"></i>
           <span className="nav-tooltip">Education</span>
-        </NavLink>
-        <NavLink to="/projects" className="nav-item" title="Projects">
+        </Link>
+        <Link
+          to="projects"
+          className="nav-item"
+          title="Projects"
+          smooth={true}
+          duration={500}
+        >
           <i className="fas fa-folder-open"></i>
           <span className="nav-tooltip">Projects</span>
-        </NavLink>
-        <NavLink to="/skills" className="nav-item" title="Skills">
+        </Link>
+        <Link
+          to="skills"
+          className="nav-item"
+          title="Skills"
+          smooth={true}
+          duration={500}
+        >
           <i className="fas fa-brain"></i>
           <span className="nav-tooltip">Skills</span>
-        </NavLink>
-        <NavLink to="/certificate" className="nav-item" title="Certificates">
+        </Link>
+        <Link
+          to="certificate"
+          className="nav-item"
+          title="Certificates"
+          smooth={true}
+          duration={500}
+        >
           <i className="fas fa-certificate"></i>
           <span className="nav-tooltip">Certificates</span>
-        </NavLink>
-        <NavLink to="/contact" className="nav-item" title="Contact">
+        </Link>
+        <Link
+          to="contact"
+          className="nav-item"
+          title="Contact"
+          smooth={true}
+          duration={500}
+        >
           <i className="fas fa-envelope-open-text"></i>
           <span className="nav-tooltip">Contact</span>
-        </NavLink>
+        </Link>
       </nav>
     </header>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Home.css";
-import { Link } from "react-router-dom";
-import { FaArrowDown } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa"; // Removed unused Link from react-router-dom
 import njg37 from "../images/njg37.jpeg";
 import Certificate from "../components/Certificate";
 import About from "../components/About";
@@ -11,10 +10,13 @@ import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
 
+// Import the Link component from react-scroll
+import { Link as ScrollLink } from "react-scroll";
+
 const Home = () => {
   return (
     <>
-      <section className="home-section">
+      <section className="home-section" id="home">
         <div className="home-container">
           {/* Left Side - Profile Image */}
           <div className="home-image">
@@ -25,19 +27,27 @@ const Home = () => {
           <div className="home-content">
             <h1 className="home-title">Hi, I'm Nabajit Ghosh</h1>
             <p className="home-description">I am a MERN Stack Developer.</p>
-            <Link to="/about" className="btn-about">
+            {/* Modify Link to use ScrollLink from react-scroll */}
+            <ScrollLink 
+              to="about" // The target section ID
+              smooth={true} // Smooth scroll effect
+              duration={500} // Duration of the scroll in ms
+              className="btn-about"
+            >
               About Me <FaArrowDown className="btn-icon" />
-            </Link>
+            </ScrollLink>
           </div>
         </div>
       </section>
-      <About />
-      <Experience />
-      <Education />
-      <Projects />
-      <Skills />
-      <Certificate />
-      <Contact />
+
+      {/* The sections with corresponding ids */}
+      <section id="about"><About /></section>
+      <section id="experience"><Experience /></section>
+      <section id="education"><Education /></section>
+      <section id="projects"><Projects /></section>
+      <section id="skills"><Skills /></section>
+      <section id="certificate"><Certificate /></section>
+      <section id="contact"><Contact /></section>
     </>
   );
 };
