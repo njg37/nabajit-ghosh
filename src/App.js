6d54a0d8833a5d0c0d4bd5db6810d3d68c686b8f
NjG19o2 <njg19o2@gmail.com>
2025-02-12 01:00:30 +0530

    enhance

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
-import Projects from './components/Projects';
-import Education from './components/Education';
-import Experience from './components/Experience';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import About from './components/About';
 import Footer from './components/Footer';
-import Certificate from './components/Certificate';
+
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
@@ -31,13 +25,6 @@ function App() {
         <main>
           <Routes>
             <Route path="/nabajit-ghosh" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/education" element={<Education />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/certificate" element={<Certificate />} />
           </Routes>
         </main>
         <Footer />
